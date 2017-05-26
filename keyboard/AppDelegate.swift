0ad788c30a91e013f9f52afb805ac8a9f0b78028
Yuki Iwanaga <yuki@creasty.com>
2017-05-26 21:41:23 +0900

    Event tap

--- keyboard/AppDelegate.swift
+++ keyboard/AppDelegate.swift
@@ -1,42 +1,72 @@
 import Cocoa
 
+func handleKeyEvent(
+    proxy: CGEventTapProxy,
+    type: CGEventType,
+    event: CGEvent,
+    refcon: UnsafeMutableRawPointer?
+) -> Unmanaged<CGEvent>? {
+    guard let ev = NSEvent(cgEvent: event) else {
+        return Unmanaged.passRetained(event)
+    }
+
+//    let flags = ev.modifierFlags
+
+    if ev.type == .keyDown {
+        if ev.keyCode == 12 /* && flags.contains(.command) */ {
+            NSWorkspace.shared().launchApplication("Finder")
+            return nil
+//            NSWorkspace.shared().launchApplication(
+//                withBundleIdentifier: "com.apple.Finder",
+//                options: [],
+//                additionalEventParamDescriptor: nil,
+//                launchIdentifier: nil
+//            )
+        }
+    }
+
+    return Unmanaged.passRetained(event)
+}
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        trustThisApplication()
+
+        let eventMask = (1 << CGEventType.keyDown.rawValue) | (1 << CGEventType.keyUp.rawValue)
+
+        guard let eventTap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: handleKeyEvent,
+            userInfo: nil
+        ) else {
+            print("Failed to create event tap")
+            exit(1)
+        }
+        
+        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: eventTap, enable: true)
+        CFRunLoopRun()
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+
+    private func trustThisApplication() {
         let opts = NSDictionary(
             object: kCFBooleanTrue,
             forKey: kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
         ) as CFDictionary
 
         guard AXIsProcessTrustedWithOptions(opts) else {
-            return
-        }
-
-        NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .keyUp]) { (event) in
-            let flags = event.modifierFlags
-
-            NSLog("%@", event)
-
-            if event.type == .keyDown {
-                if event.keyCode == 53 && flags.contains(.command) {
-                    NSWorkspace.shared().launchApplication("Finder")
-//                    NSWorkspace.shared().launchApplication(
-//                        withBundleIdentifier: "com.apple.Finder",
-//                        options: [],
-//                        additionalEventParamDescriptor: nil,
-//                        launchIdentifier: nil
-//                    )
-                }
-            }
+            exit(1)
         }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-    }
-
-
 }
-
