55b14ec151ab9e45f36f40fd70b7f673262428d4
Yuki Iwanaga <yuki@creasty.com>
2017-05-26 22:03:09 +0900

    Tweak

--- keyboard/AppDelegate.swift
+++ keyboard/AppDelegate.swift
@@ -1,5 +1,20 @@
 import Cocoa
 
+func openOrHideApplication(byBundleIdentifier id: String) {
+    let workspace = NSWorkspace.shared()
+
+    if let app = workspace.frontmostApplication, app.bundleIdentifier == id {
+        app.hide()
+    } else {
+        workspace.launchApplication(
+            withBundleIdentifier: id,
+            options: [],
+            additionalEventParamDescriptor: nil,
+            launchIdentifier: nil
+        )
+    }
+}
+
 func handleKeyEvent(
     proxy: CGEventTapProxy,
     type: CGEventType,
@@ -14,14 +29,8 @@ func handleKeyEvent(
 
     if ev.type == .keyDown {
         if ev.keyCode == 12 /* && flags.contains(.command) */ {
-            NSWorkspace.shared().launchApplication("Finder")
+            openOrHideApplication(byBundleIdentifier: "com.apple.finder")
             return nil
-//            NSWorkspace.shared().launchApplication(
-//                withBundleIdentifier: "com.apple.Finder",
-//                options: [],
-//                additionalEventParamDescriptor: nil,
-//                launchIdentifier: nil
-//            )
         }
     }
 
@@ -35,7 +44,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         trustThisApplication()
+        trapKeyEvents()
+    }
 
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+
+    func trapKeyEvents() {
         let eventMask = (1 << CGEventType.keyDown.rawValue) | (1 << CGEventType.keyUp.rawValue)
 
         guard let eventTap = CGEvent.tapCreate(
@@ -49,14 +64,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             print("Failed to create event tap")
             exit(1)
         }
-        
+
         let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
         CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
         CGEvent.tapEnable(tap: eventTap, enable: true)
         CFRunLoopRun()
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
     }
 
     private func trustThisApplication() {
