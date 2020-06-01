3e7b470da0a60a7401bf3dab0affcc7637ba5931
Yuki Iwanaga <yuki@creasty.com>
2020-06-02 02:51:21 +0900

    Implement cursor movement logic

--- keyboard/Handlers/CursorKeyHandler.swift
+++ keyboard/Handlers/CursorKeyHandler.swift
@@ -1,6 +1,17 @@
 import Cocoa
 
 final class CursorKeyHandler: Handler {
+    struct Const {
+        static let superKey: KeyCode = .c
+    }
+    
+    enum Movement {
+        case translate(x: CGFloat, y: CGFloat)
+        case translatePropotionally(rx: CGFloat, ry: CGFloat)
+        case moveTo(x: CGFloat, y: CGFloat)
+        case movePropotionallyTo(rx: CGFloat, ry: CGFloat)
+    }
+
     private let workspace: NSWorkspace
 
     init(workspace: NSWorkspace) {
@@ -8,7 +19,7 @@ final class CursorKeyHandler: Handler {
     }
 
     func activateSuperKeys() -> [KeyCode] {
-        return [.d]
+        return [Const.superKey]
     }
     
     func handle(keyEvent: KeyEvent) -> HandlerAction? {
@@ -16,11 +27,33 @@ final class CursorKeyHandler: Handler {
     }
 
     func handleSuperKey(prefix: KeyCode, keys: Set<KeyCode>) -> Bool {
-        guard prefix == .d else { return false }
+        guard prefix == Const.superKey else { return false }
 
         switch keys {
+        case [.h]:
+            moveCursor(.translate(x: -10, y: 0))
+            return true
         case [.j]:
-            moveCursor()
+            moveCursor(.translate(x: 0, y: 10))
+            return true
+        case [.k]:
+            moveCursor(.translate(x: 0, y: -10))
+            return true
+        case [.l]:
+            moveCursor(.translate(x: 10, y: 0))
+            return true
+        case [.d, .h]:
+            moveCursor(.translatePropotionally(rx: -0.1, ry: 0))
+            return true
+        case [.d, .j]:
+            moveCursor(.translatePropotionally(rx: 0, ry: 0.1))
+            return true
+        case [.d, .k]:
+            moveCursor(.translatePropotionally(rx: 0, ry: -0.1))
+            return true
+        case [.d, .l]:
+            moveCursor(.translatePropotionally(rx: 0.1, ry: 0))
+            return true
         default:
             break
         }
@@ -28,11 +61,41 @@ final class CursorKeyHandler: Handler {
         return false
     }
 
-    func moveCursor() {
-        guard let screen = NSScreen.screens.first else { return }
-        var mouseLoc = NSEvent.mouseLocation
-        mouseLoc.x = NSWidth(screen.frame) - mouseLoc.x + 100
-        mouseLoc.y = NSHeight(screen.frame) - mouseLoc.y + 100
-        CGDisplayMoveCursorToPoint(0, CGPoint(x: mouseLoc.x, y: mouseLoc.y))
+    func moveCursor(_ movement: Movement) {
+        let mouseLocation = NSEvent.mouseLocation
+
+        guard let mainScreen = NSScreen.main else { return }
+        guard let currentScreen = NSScreen.screens.first(where: { NSMouseInRect(mouseLocation, $0.frame, false) }) else { return }
+
+        var screenRect = currentScreen.frame
+        screenRect.origin.y = (mainScreen.frame.origin.y + mainScreen.frame.size.height) - (currentScreen.frame.origin.y + currentScreen.frame.size.height)
+
+        guard let voidEvent = CGEvent(source: nil) else { return }
+        var location = voidEvent.location
+//        print("CGEvent#location =", location)
+//        print("screenRect =", screenRect)
+
+        switch movement {
+        case let .translate(x, y):
+            location.x += x
+            location.y += y
+        case let .translatePropotionally(rx, ry):
+            location.x += screenRect.width * rx
+            location.y += screenRect.height * ry
+        case let .moveTo(x, y):
+            location.x = x
+            location.y = y
+        case let .movePropotionallyTo(rx, ry):
+            location.x = screenRect.minX + screenRect.width * rx
+            location.y = screenRect.minY + screenRect.height * ry
+        }
+
+        let event = CGEvent(
+            mouseEventSource: nil,
+            mouseType: .mouseMoved,
+            mouseCursorPosition: location,
+            mouseButton: .left
+        )
+        event?.post(tap: .cghidEventTap)
     }
 }
